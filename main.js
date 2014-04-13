2442102619b72d172dac644a2728ed83e770861d
codatronic <codatronic@users.noreply.github.com>
2014-04-13 11:50:53 -0500

    Added function isTrueAdd

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 // by codatronic
 // this code is subject to the included license
 // you can see a copy of it at http://codatronic.github.io/libmath.js/LICENSE.txt
+function isTrueAdd(x, y, z) {
+    var a = x + y;
+    if (a == z) {
+        return true;
+    }else{
+        return false;
+    }
+}
